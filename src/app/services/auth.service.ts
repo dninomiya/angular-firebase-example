aabd5f456b5b2ca7f5426b7953ba401d614a9e4a
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-11-22 11:39:35 +0900

    feat(authService): ログイン後リダイレクト、uid追加

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,13 +10,16 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class AuthService {
   afUser$: Observable<User> = this.afAuth.user;
+  uid: string;
 
   constructor(
     private afAuth: AngularFireAuth,
     private router: Router,
     private snackBar: MatSnackBar
   ) {
-    this.afUser$.subscribe(user => console.log(user));
+    this.afUser$.subscribe(user => {
+      this.uid = user && user.uid;
+    });
   }
 
   login() {
@@ -26,6 +29,7 @@ export class AuthService {
       this.snackBar.open(`ようこそGitPetへ！🥰`, null, {
         duration: 2000
       });
+      this.router.navigateByUrl('/create');
     });
   }
 
