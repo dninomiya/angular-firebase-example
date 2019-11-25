1636f9a041a66a36572e9457fbada566d17a2f1b
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-11-25 15:32:48 +0900

    feat(GitHubHook): 経験値アップ処理追加

--- functions/src/git-hook.function.ts
+++ functions/src/git-hook.function.ts
@@ -1,6 +1,18 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-export const gitHook = functions.https.onRequest((request, response) => {
-  console.log(request.body);
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const gitHook = functions.https.onRequest(async (request, response) => {
+  const pets = await db.collection('pets')
+    .where('ownerGitHubId', '==', request.body.sender.id)
+    .get();
+
+  const increement = admin.firestore.FieldValue.increment(10);
+  pets.docs.forEach(doc => doc.ref.update({
+    exp: increement
+  }));
   response.send('success');
 });
